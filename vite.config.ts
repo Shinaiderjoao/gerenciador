2c9e580346fc84997fc16f59ba2d0f97e7250e08
Joao <shinaidervictor15@gmail.com>
2025-02-23 13:19:24 -0300

    Att

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createServer } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
